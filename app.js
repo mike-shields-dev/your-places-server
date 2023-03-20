8f07621ae1846729e097615194ca7687703b3635
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-20 13:56:13 +0000

    fix: Configure bodyParser to process JSON data

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import usersRoute from './routes/users/route.js';
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api/places', placesRoute);
 app.use('/api/users', usersRoute);
