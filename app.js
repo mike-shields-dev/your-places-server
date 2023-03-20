3c9069a992c6afe86e0cdc5b65ebb7987ba30fbb
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-20 12:07:46 +0000

    refactor: Format app

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const PORT = process.env.PORT || 5000;
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-const app = express();
 
 import placesRoute from './routes/places/route.js';
 import usersRoute from './routes/users/route.js';
 
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/api/places', placesRoute);
 app.use('/api/users', usersRoute);
 
