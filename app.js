cfd4bfa081306004332a15e2bf59da0f313e0f36
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-20 09:43:55 +0000

    chore: Add cors to app

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
