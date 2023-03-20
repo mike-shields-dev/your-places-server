862c9ccd835b1b4498467b4025c7728fed10d7a6
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-20 10:46:06 +0000

    refactor: Update to import statements and file restructure

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const PORT = process.env.PORT || 5000;
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 const app = express();
 
-const placesRoute = require('./routes/places');
+import placesRoute from './routes/places/route.js';
+import usersRoute from './routes/users/route.js';
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/places', placesRoute);
+app.use('/api/users', usersRoute);
 
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
--- routes/places/route.js
+++ routes/places/route.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
 
 const PLACES = [
@@ -41,4 +41,4 @@ router.get('/:pid', (req, res) => {
         .json({ message: 'No place found' });
 });
 
-module.exports = router;
+export default router;
