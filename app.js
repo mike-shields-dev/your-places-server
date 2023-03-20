c4e6ab5920b2e567ba5be78fb6eb92e3a0ec4ca8
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-20 12:40:30 +0000

    feat: Add error handling middleware to catch all errors

--- app.js
+++ app.js
@@ -15,4 +15,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/places', placesRoute);
 app.use('/api/users', usersRoute);
 
-app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+
+    res.status(error.code || 500)
+        .json({
+            message:
+                error.message ||
+                'An unknown error occurred!',
+        });
+});
+
+app.listen(
+    PORT,
+    () => console.log(`App is listening on ${PORT}`)
+);
