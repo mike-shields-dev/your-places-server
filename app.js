3c921ec539005d674daf17f519e50ce1f8e21387
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-20 09:00:51 +0000

    feat: Create app.js, main entry point to server app

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Test");
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(() => console.log(`App is listening on ${PORT}`));
