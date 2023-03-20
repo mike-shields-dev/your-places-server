fd4f989ce5572e51909239baeac4adefbb0ad112
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-20 15:48:08 +0000

    refactor/fix: Copy imported data to places variable so that it can be redefined set res.sendStatus(204) in deletePlaceByPlaceId

--- controllers/places/controller.js
+++ controllers/places/controller.js
@@ -2,6 +2,8 @@ import { HttpError, errorMsg } from '../../models/http-error.js';
 import data from './places-data.json' assert { type: "json" };
 import { v4 as uuid } from 'uuid';
 
+let places = data;
+
 const createPlace = (req, res, next) => {
     const {
         title,
@@ -20,14 +22,14 @@ const createPlace = (req, res, next) => {
         creator,
     };
 
-    data.push(newPlace);
+    places.push(newPlace);
 
     res.status(201).json({ place: newPlace });
 };
 
 const getPlaceByPlaceId = (req, res, next) => {
     const place =
-        data.find(({ id }) => id === req.params.pid);
+        places.find(({ id }) => id === req.params.pid);
 
     if (place) {
         return res.json(place);
@@ -37,11 +39,11 @@ const getPlaceByPlaceId = (req, res, next) => {
 }
 
 const getPlacesByUserId = (req, res, next) => {
-    const places =
-        data.filter(({ creator }) => creator === req.params.uid);
+    const usersPlaces =
+        places.filter(({ creator }) => creator === req.params.uid);
 
-    if (places.length) {
-        return res.json(places);
+    if (usersPlaces.length) {
+        return res.json(usersPlaces);
     }
     
     next(new HttpError(errorMsg('places', 'user'), 404));
@@ -50,34 +52,35 @@ const getPlacesByUserId = (req, res, next) => {
 const updatePlaceByPlaceId = (req, res, next) => {
     const { pid } = req.params;
     const { title, description } = req.body;
-    const foundIndex = data.findIndex(({ id }) => id === pid)
+    const foundIndex = places.findIndex(({ id }) => id === pid)
 
     if (foundIndex < 0) {
         return next(new HttpError(errorMsg('place', 'place'), 404));    
     }
 
-    data[foundIndex] = {
-        ...data[foundIndex],
+    places[foundIndex] = {
+        ...places[foundIndex],
         title,
         description,
     };
 
     return res
         .status(200)
-        .json({ place: data[foundIndex] });
+        .json({ place: places[foundIndex] });
 };
 
 const deletePlaceByPlaceId = (req, res, next) => {
     const { pid } = req.params;
-    const place = data.find(place => place.id === pid)
+    
+    const foundPlace = places.find(({ id }) => id === pid)
 
-    if (!place) {
+    if (!foundPlace) {
         return next(new HttpError(errorMsg('place', 'place'), 404));
     }
 
-    data = data.filter(place => place.id !== pid);
+    places = places.filter(({ id }) => id !== pid);
 
-    return res.status(204);  
+    return res.sendStatus(204);  
 };
 
 export {
